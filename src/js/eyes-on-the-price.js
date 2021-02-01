4cbce0014efb7c02c3b9b20acff36e6947fd66f4
Daniel Wilches <wilches@google.com>
2021-02-01 17:48:50 -0500

    Added follow ball function and first Styling for the experiment

--- src/js/eyes-on-the-price.js
+++ src/js/eyes-on-the-price.js
@@ -1,4 +1,40 @@
 // shared module
 import '../scss/eyes-on-the-price.scss';
 
-console.log('First Excersise ME');
+/**
+ * Selectors
+ * @enum {string}
+ */
+const SELECTORS = {
+  BALL: '.ball',
+  WRAPPER: '.follow-ball',
+};
+
+/**
+ * Configuration
+ * @enum {number}
+ */
+const CONF = {
+  BALL: {
+    FOLLOW_DELAY: 0,
+  },
+};
+
+/** @type {HTMLElement} */
+const $wrapper = document.querySelector(SELECTORS.WRAPPER);
+/** @type {HTMLElement} */
+const $ball = document.querySelector(SELECTORS.BALL);
+
+/**
+ * Makes the ball follow the mouse
+ *
+ * @param {event} event
+ */
+function followBall(event) {
+  setTimeout(() => {
+    $ball.style.left = `${event.pageX}px`;
+    $ball.style.top = `${event.pageY}px`;
+  }, CONF.BALL.FOLLOW_DELAY);
+}
+
+$wrapper.addEventListener('mousemove', followBall);
